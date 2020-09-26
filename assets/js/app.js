041c2fc2111189ec0ac177e4d5ad7e5cca6c1502
Kevin Avery <Averykev@gmail.com>
2020-09-26 15:52:50 -0700

    Updated x and y labels

--- assets/js/app.js
+++ assets/js/app.js
@@ -75,7 +75,89 @@ d3.csv("assets/data/data.csv").then(function(newsData, err){
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
+    //append x-axis
+    var xAxis = chartGroup.append("g")
+            .classed("x-axis", true)
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+
+    //append y-axis
+    var yAxis = chartGroup.append("g")
+            .call(leftAxis);
+
     
+    //append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+            .data(newsData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScale(d[chosenXAxis]))
+            .attr("cy", d => yLinearScale(d[chosenYAxis]))
+            .attr("r", 20)
+            .classed("stateCircle", true);
+
+    //create group for three x-axis labels
+    var xlabelsGroup = chartGroup.append("g")
+            .attr("transform", `translate(${width / 2}, ${height +20})`);
+
+    var povertyLabel = xlabelsGroup.append("text")
+            .attr("x", 0)
+            .attr("y", 20)
+            .attr("value", "poverty") // value to grab for event listener
+            .classed("active", true)
+            .text("In Poverty (%)");
+
+    var ageLabel = xlabelsGroup.append("text")
+            .attr("x", 0)
+            .attr("y", 40)
+            .attr("value", "num_albums") // value to grab for event listener
+            .classed("inactive", true)
+            .text("Age (Median)");
+
+    var incomeLabel = xlabelsGroup.append("text")
+            .attr("x", 0)
+            .attr("y", 60)
+            .attr("value", "num_albums") // value to grab for event listener
+            .classed("inactive", true)
+            .text("Household Income (Median)");
+
+    //create group for three y-axis labels
+    var yLabelsGroup = chartGroup.append("g")
+            .attr("transform", "rotate(-90)");
+
+    var obesityLabel = yLabelsGroup.append("text")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .classed("active", true)
+            .classed("aText", true)
+            .text("Obese (%)")
+
+    var smokesLabel = yLabelsGroup.append("text")
+            .attr("y", 0 - margin.left + 20)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .classed("inactive", true)
+            .classed("aText", true)
+            .text("Smokes (%)")
+
+    var healthcareLabel = yLabelsGroup.append("text")
+            .attr("y", 0 - margin.left + 40)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .classed("inactive", true)
+            .classed("aText", true)
+            .text("Lacks Healthcare (%)")
+
+
+    //updateToolTip function from above function list
+
+
+
+
+
+
+
 
 
     
